1346413c9fe33d170b05c6910aacac9fd7bbf727
andrewdoss <doss@bit.io>
2021-09-09 14:31:27 -0700

    Finished Python examples

--- python/upload_data.py
+++ python/upload_data.py
@@ -7,12 +7,13 @@ b = bitdotio.bitdotio(<YOUR_BITIO_KEY>)
 
 # From file
 # Download demo csv here:
-# https://docs.bit.io/docs/uploading-data#:~:text=video-game-sales.csv
+# https://raw.githubusercontent.com/bitdotioinc/connection_snippets/main/video-game-sales.csv
+# Upload to 'my_new_repo' with table name 'video_game_sales_csv'
 with open('video-game-sales.csv', 'r', encoding='utf-8') as f:
-    b.create_import_file(f, 'my_new_repo', table_name='video_game_sales_csv')
+    b.create_import_file(f, 'my_new_repo', 'video_game_sales_csv')
 
 # From URL
-import_url = bitdotio.model.import_url.ImportUrl('https://storage.googleapis.com/bitdotio-demo-datasets/video-game-sales.csv',
-                                                 'video_game_sales_url', 
-                                                 'my_new_repo')
+# Upload to 'my_new_repo' with table name 'video_game_sales_url'
+url = 'https://raw.githubusercontent.com/bitdotioinc/connection_snippets/main/video-game-sales.csv'
+import_url = bitdotio.model.import_url.ImportUrl(url, 'video_game_sales_url', 'my_new_repo')
 b.create_import_url(import_url=import_url)
