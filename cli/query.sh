ef20ddf3a29becdf23d316d19713b847417d65d2
andrewdoss <doss@bit.io>
2021-09-14 09:11:28 -0700

    Shortened CLI queries

--- cli/query.sh
+++ cli/query.sh
@@ -1,5 +1,8 @@
 # Query from a bit.io repo using the CLI
 
-bit -k <YOUR_BITIO_KEY> query -q "SELECT date, name, local_price FROM \"bitdotio/big_mac_index\".\"big_mac_index\"" 
+SQL="SELECT date, name, local_price FROM \"bitdotio/big_mac_index\".\"big_mac_index\"" 
+bit -k <YOUR_BITIO_KEY> query -q $SQL
 
-echo  "SELECT date, name, local_price FROM \"bitdotio/big_mac_index\".\"big_mac_index\"" | bit -k <YOUR_BITIO_KEY> query -qf -
+# Pipe a query to bit
+echo  "SELECT date, name, local_price FROM \"bitdotio/big_mac_index\".\"big_mac_index\"" \
+| bit -k <YOUR_BITIO_KEY> query -qf -
